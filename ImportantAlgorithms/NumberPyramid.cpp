2dccbb0e324fc291122b114751c05335a3b5aa68
Datla Rohit Varma <visaal1582892@gmail.com>
2023-03-11 10:36:12 +0530

    new file added into ImportantAlgorithms

---
+++ ImportantAlgorithms/NumberPyramid.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int n;
+    cout<<"enter number of lines of pattern you want : ";
+    cin>>n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n-1; j >= i ; j--)
+        {
+            cout<<" ";
+        }
+        int k=i;
+        while (k<=i+(i-1))
+        {
+            cout<<k;
+            k+=1;
+        }
+        int l=k-2;
+        while (l>=i)
+        {
+            cout<<l;
+            l-=1;
+        }
+        for (int j = n-1; j >= i ; j--)
+        {
+            cout<<" ";
+        }
+        cout<<"\n";
+    }
+    
+    return 0;
+}
